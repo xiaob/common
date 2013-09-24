406cff3170d473b4f23d861eaee3319720cc6f5f
yuancihang <yuancihang@tom.com>
2013-09-24 18:43:48 +0800

    浮点数

---
+++ src/tmp/algorithm/book/chapter1/Test1.java
@@ -0,0 +1,19 @@
+package tmp.algorithm.book.chapter1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		test1();
+
+	}
+	
+	// 浮点数字面量
+	public static void test1(){
+		double f1 = 10e15;
+		System.out.println(f1);
+		
+		double f2 = 10e-15;
+		System.out.println(f2);
+	}
+
+}
