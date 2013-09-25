374b6b0ad0952cfbadb9b479e7c27e3a6f242aa1
yuancihang <yuancihang@tom.com>
2013-09-25 18:52:50 +0800

    矩阵的乘法

--- src/tmp/algorithm/book/chapter1/Test1.java
+++ src/tmp/algorithm/book/chapter1/Test1.java
@@ -16,4 +16,30 @@ public class Test1 {
 		System.out.println(f2);
 	}
 
+	// 矩阵相乘
+	public static void test2(){
+		int[][] a = new int[3][2];
+		int[][] b = new int[2][3];
+		
+		initArray(a);
+		initArray(b);
+		
+		for(int i=0; i<a.length; i++){
+			int sum = 0;
+			for(int j=0; j<a[i].length; j++){
+				for(int k=0; k<b.length; k++){
+					sum += a[i][j] * b[k][i];
+				}
+			}
+//			System.out.println(x);
+		}
+	}
+	private static void initArray(int[][] x){
+		for(int i=0; i<x.length; i++){
+			for(int j=0; j<x[i].length; j++){
+				x[i][j] = 1;
+			}
+		}
+	}
+	
 }
