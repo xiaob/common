f8644b3e5a0c13436801abe63d3fe578e3ed35bc
yuancihang <yuancihang@tom.com>
2013-12-31 10:03:08 +0800

    搜索

--- src/tmp/algorithm/Search.java
+++ src/tmp/algorithm/Search.java
@@ -6,17 +6,21 @@ import java.util.Random;
 public class Search {
 
 	public static void main(String[] args) {
-		int[] a = random(200);
+		int[] a = random(10);
 		Arrays.sort(a);
 		
 		long start = System.currentTimeMillis();
-		System.out.println(binarySearch(a, a[13]));
+		System.out.println(binarySearch(a, a[3]));
 		long end = System.currentTimeMillis();
 		
 		System.out.println("耗时：" + (end - start) + "ms, ");
 		
 		System.out.println(hash("111"));
 		System.out.println(hash("222"));
+		
+		System.out.println(Arrays.toString(a));
+		// 查找中位数
+		System.out.println(quickSearch(a, a.length/2));
 	}
 	
 	private static int[] random(int size){
@@ -60,6 +64,63 @@ public class Search {
 		}
 		
 		return hash;
+	}
+	
+	// 查找第K小的数
+	public static int quickSearch(int[] a, int k){
+		if((k < 0) || (k > a.length)){
+			return -1;
+		}
+		
+		return quickSearch(a, 0, a.length - 1, k);
+	}
+	private static int quickSearch(int[] a, int beginIndex, int endIndex, int k){
+		if(beginIndex > endIndex){
+			return -1;
+		}
+		
+		int left = beginIndex;
+		int right = endIndex;
+		int base = a[left];
+		
+		// 直到left指针与right指针重合, 一轮排序才结束
+		while(left < right){
+			// 向前查找直到找到比base小的数, 然后赋值给left位置
+			while(left < right){
+				if(a[right] >= base){
+					right --;
+				}else{
+					a[left] = a[right];
+					break;
+				}
+			}
+			// 向后查找直到找到比base大的数, 然后赋值给right位置
+			while(left < right){
+				if(a[left] <= base){
+					left ++;
+				}else{
+					a[right] = a[left];
+					break;
+				}
+			}
+		}
+		// 最后将base插入到left位置
+		a[left] = base;
+		
+		// 以left位置为分割点, 递归查找子数组
+		if((k-1+beginIndex) < left){
+			if((left - 1) >= beginIndex){
+				return quickSearch(a, beginIndex, left - 1, k);
+			}
+			return -1;
+		}else if((k-1+beginIndex) > left){
+			if((left + 1) <= endIndex){
+				return quickSearch(a, left + 1, endIndex, (k-1+beginIndex) - left);
+			}
+			return -1;
+		}else{
+			return a[left]; // 刚好是中间点
+		}
 	}
 	
 }
