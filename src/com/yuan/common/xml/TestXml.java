58f9126b0f6e3b613692c5c9e1f78eced49e3a65
yuancihang <yuancihang@tom.com>
2014-04-11 19:13:31 +0800

    修改xml

--- src/com/yuan/common/xml/TestXml.java
+++ src/com/yuan/common/xml/TestXml.java
@@ -6,7 +6,8 @@ import java.util.Date;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlList;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
@@ -24,22 +25,25 @@ public class TestXml {
 	}
 	
 	public static void test2()throws Exception{
-		Jaxb.write(new User(), "d:/jaxb.xml");
+//		Jaxb.write(new User(), "d:/jaxb.xml");
+		User user = Jaxb.parse(User.class, new FileInputStream("d:/jaxb.xml"));
+		System.out.println(user.getSex());
+		System.out.println(user.getBirthday());
+		System.out.println(user.getStus().get(0).getName());
 	}
 
 }
 
 @XmlRootElement
 class User{
-	@XmlAttribute
 	private String sex="man";
 	
-	@XmlAttribute
 	private int age=20;
 	
 	private String birthday="wang";
 	
 	private List<String> addresses = new ArrayList<String>();
+	private List<Stu> stus = new ArrayList<Stu>();
 	
 	private Date date = new Date();
 
@@ -47,6 +51,8 @@ class User{
 		super();
 		addresses.add("111");
 		addresses.add("222");
+		stus.add(new Stu(1, "zhang"));
+		stus.add(new Stu(2, "li"));
 	}
 
 	public User(String sex, int age, String birthday) {
@@ -56,22 +62,24 @@ class User{
 		this.birthday = birthday;
 	}
 	
-//	public String getSex() {
-//		return sex;
-//	}
-//
-//	public void setSex(String sex) {
-//		this.sex = sex;
-//	}
-//
-//	public int getAge() {
-//		return age;
-//	}
-//
-//	public void setAge(int age) {
-//		this.age = age;
-//	}
-//
+	@XmlAttribute
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	@XmlAttribute
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 	public String getBirthday() {
 		return birthday;
 	}
@@ -80,7 +88,7 @@ class User{
 		this.birthday = birthday;
 	}
 
-	@XmlList
+//	@XmlList
 	public List<String> getAddresses() {
 		return addresses;
 	}
@@ -96,6 +104,45 @@ class User{
 
 	public void setDate(Date date) {
 		this.date = date;
+	}
+	
+	@XmlElementWrapper(name="stus")
+	@XmlElement(name="stu") 
+	public List<Stu> getStus() {
+		return stus;
+	}
+
+	public void setStus(List<Stu> stus) {
+		this.stus = stus;
+	}
+
+	public static class Stu{
+		private int id;
+		private String name;
+		
+		public Stu(int id, String name) {
+			super();
+			this.id = id;
+			this.name = name;
+		}
+		
+		public Stu() {
+			super();
+		}
+
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		
 	}
 
 }
