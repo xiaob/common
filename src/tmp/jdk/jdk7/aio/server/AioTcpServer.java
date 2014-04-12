3a241fd048c1591bfb116f2e9fa1ec3130540086
yuancihang <yuancihang@tom.com>
2014-04-13 01:10:16 +0800

    aio

---
+++ src/tmp/jdk/jdk7/aio/server/AioTcpServer.java
@@ -0,0 +1,34 @@
+package tmp.jdk.jdk7.aio.server;
+
+import java.net.InetSocketAddress;
+import java.nio.channels.AsynchronousChannelGroup;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+ 
+public class AioTcpServer implements Runnable { 
+    private AsynchronousChannelGroup asyncChannelGroup;  
+    private AsynchronousServerSocketChannel listener;  
+  
+    public AioTcpServer(int port) throws Exception { 
+        ExecutorService executor = Executors.newCachedThreadPool(); 
+        asyncChannelGroup = AsynchronousChannelGroup.withThreadPool(executor); 
+        listener = AsynchronousServerSocketChannel.open(asyncChannelGroup).bind(new InetSocketAddress(port)); 
+    } 
+ 
+    public void run() { 
+        try { 
+            listener.accept(listener, new AioAcceptHandler());  
+            Thread.sleep(400000); 
+        } catch (Exception e) { 
+            e.printStackTrace(); 
+        } finally { 
+         System.out.println("finished server");
+        } 
+    } 
+ 
+    public static void main(String... args) throws Exception { 
+        AioTcpServer server = new AioTcpServer(9008); 
+        new Thread(server).start(); 
+    } 
+}
